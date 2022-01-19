487ce41a07bb29802da28f532844dc18e94cab24
Herve Chendjou <hchend2@uic.edu>
2022-01-19 01:37:10 -0600

    iterator added

--- myvector.h
+++ myvector.h
@@ -18,7 +18,43 @@ class myvector {
         T* ptr; // dynamically allocated c-like array...
         int size; // number of elements in array...
         int capacity; // total number of locations available... 
-        
+
+        class iterator {
+            private:
+                myvector* v; // the vector we are iterating over...
+                int curIndex; // the element we are currently at...
+            public:
+                // default constructor...denotes the 1st item...
+               iterator(myvector* v) {
+                   this->v = v; //
+                   curIndex = 0; //
+               }
+               // constructor...denotes the nth item...
+               iterator(myvector* v, int n) {
+                   this->v = v;
+                   curIndex = n;
+               }
+               // advance to the next...
+               iterator& operator++() {
+                   curIndex++;
+                   return *this;
+               }
+               // access current element...
+               T& operator*() {
+                   return v->ptr[curIndex];
+               }
+               // compare to stop iterating...
+               bool operator != (const iterator& rhs) {
+                   if (v != rhs.v) { // different vectors...
+                       return true;
+                   }
+                   if (curIndex != rhs.curIndex) { // different index...
+                       return true;
+                   }
+                    // same underlying vector and index, so iterator denotes same element...
+                   return false
+               }
+        };
     public:
         myvector() {
             capacity = 4;
